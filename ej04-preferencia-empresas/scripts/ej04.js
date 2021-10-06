6cb5bab85ce6a1f29a817eb1707a7f2ef8a08085
LuisMiguelFC <luismiguel.fernandez@murciaeduca.es>
2021-10-07 01:38:29 +0200

    ej04 con arrays

---
+++ ej04-preferencia-empresas/scripts/ej04.js
@@ -0,0 +1,14 @@
+//datos versión fácil
+let empresas = ["Google","Facebook","Apple","Everis","Twitter","Microsoft"]
+
+//datos versión menos fácil
+/*
+let empresas = [
+    {name: "Google", code: "goo"},
+    {name: "Facebook", code: "fac"},
+    {name: "Apple", code: "app"},
+    {name: "Everis", code: "eve"},
+    {name: "Twitter", code: "twi"},
+    {name: "Microsoft", code: "mic"}
+]
+*/
