c79059b0da172b17f48addd896502e5e4d10430b
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2021-10-07 10:17:28 +0200

    ej04 en construccion

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Ejercicios sencillos para aprender las características del lenguaje Javascript
 ej00: plantilla para ahorrar tiempo al crear nuevos ejercicios desde cero
 ej01: juego simple sobre adivinar un número del 1 al 100 con un máximo de 6 intentos
 ej02: creación y filtrado de un listado en formato tabla a partir de un array de elementos
-ej03: 
+ej03: manipulación de arrays asociativos y elementos SELECT y OPTION
+ej04: formularios, submit, SELECT/OPTION, tablas
--- ej04-preferencia-empresas/scripts/ej04.js
+++ ej04-preferencia-empresas/scripts/ej04.js
@@ -1,8 +1,7 @@
 //datos versión fácil
-let empresas = ["Google","Facebook","Apple","Everis","Twitter","Microsoft"]
+//let empresas = ["Google","Facebook","Apple","Everis","Twitter","Microsoft"]
 
 //datos versión menos fácil
-/*
 let empresas = [
     {name: "Google", code: "goo"},
     {name: "Facebook", code: "fac"},
@@ -11,4 +10,27 @@ let empresas = [
     {name: "Twitter", code: "twi"},
     {name: "Microsoft", code: "mic"}
 ]
-*/
+
+const selectEmpresas1 = document.querySelector("#empresa1")
+const selectEmpresas2 = document.querySelector("#empresa2")
+
+for (let empresa of empresas) {
+    let nuevoOption = document.createElement("OPTION")
+    nuevoOption.textContent = empresa.name
+    nuevoOption.value = empresa.code
+    selectEmpresas1.append(nuevoOption)
+}
+
+selectEmpresas1.addEventListener("change",function(){
+    //El usuario ha elegido empresa en el primer SELECT
+    console.log(this.value)
+    // poblar el segundo SELECT
+    for (let empresa of empresas) {
+        let nuevoOption = document.createElement("OPTION")
+        nuevoOption.textContent = empresa.name
+        nuevoOption.value = empresa.code
+        selectEmpresas2.append(nuevoOption)
+    }
+    // habilitar el segundo SELECT
+    selectEmpresas2.disabled = false
+})
