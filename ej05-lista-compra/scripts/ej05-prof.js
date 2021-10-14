cb65e22cdf7e01bfb500f6ee450c349929422c77
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2021-10-14 10:14:14 +0200

    ej05 terminado

--- ej05-lista-compra/scripts/ej05-prof.js
+++ ej05-lista-compra/scripts/ej05-prof.js
@@ -7,6 +7,9 @@ const myCart = document.querySelector("#mycart")
 const btnSelAll = document.querySelector("#btnSelAll")
 const btnSelNot = document.querySelector("#btnSelNot")
 const btnInvSel = document.querySelector("#btnInvSel")
+const btnMovSel = document.querySelector("#btnMovSel")
+const btnDelSel = document.querySelector("#btnDelSel")
+const btnEmpCar = document.querySelector("#btnEmpCar")
 
 //función que añade a la lista lo escrito en el INPUT
 function addToList(){
@@ -21,6 +24,11 @@ function addToList(){
         //vaciar el INPUT y darle el foco
         inputText.value = ""
         inputText.focus()
+        //hacer clicable al LI
+        /* ANULADO PORQUE LO RESOLVEMOS CON DELEGACIÓN DE EVENTOS
+        newLI.addEventListener("click", function(){
+            this.classList.toggle("seleccionado")
+        })*/
     }
 }
 
@@ -53,3 +61,46 @@ btnInvSel.addEventListener("click", function() {
         element.classList.toggle("seleccionado")
     })
 })
+
+//programar el comportamiento del botón "btnMovSel"
+btnMovSel.addEventListener("click", function() {
+    Array.from(myList.children).forEach(element => {
+        if (element.classList.contains("seleccionado")) {
+            myCart.append(element)
+            element.classList.remove("seleccionado")
+        }
+    })
+})
+
+//programar el comportamiento del botón "btnDelSel"
+btnDelSel.addEventListener("click", function() {
+   let seleccionados = myList.querySelectorAll("li.seleccionado")
+   seleccionados.forEach(element => element.remove())
+})
+
+//programar el comportamiento del botón "btnEmpCar"
+btnEmpCar.addEventListener("click", function() {
+    myCart.innerHTML = ""
+})
+
+//programar UL#mylist para que capture el click sobre los LI
+myList.addEventListener("click", function(ev){
+    if (ev.target.nodeName == "LI")
+        ev.target.classList.toggle("seleccionado")
+})
+
+
+/*
+//Recorrer este array de ejemplo y hacer algo con cada elemento
+seleccionados = ["Miguel", "Luismi", "Pepe"]
+//solucion1
+seleccionados.forEach(element => element.remove())
+//solucion2
+seleccionados.forEach(function(element) {
+    element.remove()
+})
+//solucion3
+for (let i=1; i<seleccionados.length; i++) {
+    seleccionados[i].remove()
+}
+*/
