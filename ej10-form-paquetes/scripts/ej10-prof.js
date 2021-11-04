c624b7fd7e0e5985409c2997e9f30ae093028bfd
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2021-11-04 10:20:01 +0100

    ej10 casi terminado

--- ej10-form-paquetes/scripts/ej10-prof.js
+++ ej10-form-paquetes/scripts/ej10-prof.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded",main)
-const añadir=document.querySelector("#anyadir")
+const form = document.querySelector("#formEnvios")
+const anyadir = document.querySelector("#anyadir")
+const pedido=document.querySelector("#refPedido")
+const peso=document.querySelector("#peso")
+
 function main(){
-    const form = document.querySelector("#formEnvios")
+    
     form.action = "todoOK.html" //forma rápida
     //form.setAttribute("action","todoOK.html") //forma alternativa
     form.addEventListener("submit",function(ev){
@@ -10,26 +14,31 @@ function main(){
             this.submit()
         }
     })
+
+    anyadir.addEventListener("click",function(){
+        if (checkForm()) {
+            addToTable(pedido.value.trim() , peso.value.trim())
+        }
+    })
+
 }
 
 function checkForm(){
-let todoOK=true
-const pedido=document.querySelector("#refPedido")
-const peso=document.querySelector("#peso")
-const pedidoError=document.querySelector("#refPedidoError")
-const pedidoPeso=document.querySelector("#pesoError")
-if(!checkPedido(pedido.value.trim())){
-    todoOK=false
-    pedidoError.textContent="La referencia tiene el formato A1234"
-    pedido.classList.add("campoIncorrecto")
+    let todoOK=true
+    const pedidoError=document.querySelector("#refPedidoError")
+    const pedidoPeso=document.querySelector("#pesoError")
+    if(!checkPedido(pedido.value.trim())){
+        todoOK=false
+        pedidoError.textContent="La referencia tiene el formato A1234"
+        pedido.classList.add("campoIncorrecto")
     }else{
         pedido.classList.remove("campoIncorrecto")
         pedidoError.textContent=""
     }
-if(!checkPeso(peso.value.trim())){
-    todoOK=false
-    pedidoPeso.textContent="El peso tiene el formato 1.1 o 11.1"
-    peso.classList.add("campoIncorrecto")
+    if(!checkPeso(peso.value.trim())){
+        todoOK=false
+        pedidoPeso.textContent="El peso tiene el formato 1.1 o 11.1"
+        peso.classList.add("campoIncorrecto")
     }else{
         peso.classList.remove("campoIncorrecto")
         pedidoPeso.textContent=""
@@ -43,9 +52,23 @@ function checkPedido(pedido){
 }
 
 function checkPeso(peso){
-    let expReg = /^\d{1,2}.\d$/
-    return (expReg.test(peso))
+    let expReg1 = /^\d{1,2}.\d$/    //desde 0.0 hasta 99.9
+    let expReg2 = /^0+.0$/          //solamente 0.0 y 00.0
+    return (expReg1.test(peso) && !expReg2.test(peso))
 }
-añadir.addEventListener("click",function(){
-    
-})
+
+function addToTable(pedido, peso) {
+    let tbody = document.querySelector("#tablaEnvios>tbody")
+    let newRow = tbody.insertRow()
+    let cell1 = newRow.insertCell()
+    let cell2 = newRow.insertCell()
+    let cell3 = newRow.insertCell()
+    cell1.textContent = pedido.toUpperCase()
+    cell2.textContent = peso
+    let deleteRowButton = document.createElement("button")
+    deleteRowButton.textContent = "X"
+    deleteRowButton.addEventListener("click",()=>{
+        newRow.remove()
+    })
+    cell3.append(deleteRowButton)
+}
